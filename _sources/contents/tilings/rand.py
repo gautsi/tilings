8ab25f8e4f73f45618d7540164cada13d7933af9
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-11-23 17:58:45 -0500

    Update documentation

--- _sources/contents/tilings/rand.py
+++ _sources/contents/tilings/rand.py
@@ -20,6 +20,7 @@ from tilings import base as b
 import matplotlib.pyplot as plt
 import shapely.geometry as sg
 import numpy as np
+from myst_nb import glue
 
 # %%
 triangle_pts = [sg.Point(i) for i in [[0,0], [-0.5, np.sqrt(3)/2], [0.5, np.sqrt(3)/2]]]
@@ -28,9 +29,9 @@ seed_polys = [sg.Polygon(pts) for pts in u.repeat(triangle_pts)[0] + u.repeat(sq
 seed_t = b.Tiling(polys=seed_polys, u=u.union(seed_polys))
 
 # %%
-fig, ax = u.setup_plot(extent=5)
+fig, ax = u.setup_plot(extent=2)
 u.draw_tiling(ax, seed_t)
-
+glue("seed", fig)
 
 # %%
 fig, ax = u.setup_plot(extent=5)
@@ -40,7 +41,7 @@ u.draw_tiling(ax, u.update_tilings([seed_t])[0])
 
 # %%
 ts = [seed_t]
-for i in range(36):
+for i in range(2):
     ts = u.update_tilings(ts)
     print(i, len(ts))
     irreg = u.get_irreg(ts)
@@ -49,15 +50,42 @@ for i in range(36):
         break
 
 # %%
-for t in ts[:10]:
-    fig, ax = u.setup_plot(extent=10)
+ts = u.update_tilings(ts)
+print(i, len(ts))
+
+
+# %%
+for t in ts:
+    fig, ax = u.setup_plot(extent=6)
     u.draw_pts(ax, u.nearest_edge(t.u))
     u.draw_tiling(ax, t)
 
 # %%
-for t in ts[-10:]:
-    fig, ax = u.setup_plot(extent=10)
+new_seed = ts[2]
+
+# %%
+ts2 = [new_seed]
+for i in range(23):
+    ts2 = u.update_tilings(ts2)
+    print(i, len(ts2))
+    irreg = u.get_irreg(ts2)
+    if irreg is not None:
+        print("irreg found")
+        break
+
+
+# %%
+for t in ts2:
+    fig, ax = u.setup_plot(extent=6)
     u.draw_pts(ax, u.nearest_edge(t.u))
     u.draw_tiling(ax, t)
+
+# %%
+glue("shapely_ex", fig)
+
+# %%
+fig, ax = u.setup_plot(extent=6)
+u.draw_polygon(ax, t.u)
+glue("artifact_ex", fig)
 
 # %%
